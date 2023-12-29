e51682105ec4ec1ce89967a27b396ed98f9b524a
guoqi.sun <sun412926@gmail.com>
2023-12-29 14:50:55 +0800

    :bug: fix: remove git-hooks

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,12 @@
     <div
       text="44px"
       color="blue"
-      bg="red"
+      bg="red-500"
     >
       Hello World
-
-
-      
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.test {
-  .test1 {
-    color: red;
-  }
-}
 </style>
